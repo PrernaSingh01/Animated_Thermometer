3fd05d584e70cd6307894921963c641f9a2438b1
Prerna <iprernasingh4@gmail.com>
2023-06-28 20:37:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const tempLoad = () => {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = "&#xf2cb;";
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+  }, 1000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9;";
+    temp.style.color = "yellow";
+  }, 2000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8;";
+    temp.style.color = "orange";
+  }, 3000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7;";
+    temp.style.color = "red";
+  }, 4000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2cb";
+    temp.style.color = "white";
+  }, 5000);
+};
+
+tempLoad();
+
+setInterval(tempLoad, 5000);
